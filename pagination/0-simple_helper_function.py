2fcb924170f49006de50e76fa8bfa25e1935d9f6
Ravou <oliviaravou@hotmail.fr>
2025-08-12 15:09:29 +0200

    fix pycodestyle again

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 
 def index_range(page, page_size):
-    
+
     """
     Calculate the start and end index for pagination.
 
     Return a tuple of two integers (start_index, end_index)
-
     """
-
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
