0355f2830d1e2a89e08eea13852bc072e99ae363
Ravou <oliviaravou@hotmail.fr>
2025-08-12 15:06:45 +0200

    add documentation

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
 def index_range(page, page_size):
+    
+    """
+    Calculate the start and end index for pagination.
+
+    Return a tuple of two integers (start_index, end_index)
+
+    """
 
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
