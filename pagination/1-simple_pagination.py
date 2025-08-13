fb8bb3a83b7f8aecb3cd8981c6152135fe1c83c1
Ravou <oliviaravou@hotmail.fr>
2025-08-13 11:21:53 +0200

    fix pycodestyle

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -6,7 +6,6 @@ from typing import List
 
 
 def index_range(page, page_size):
-
     """
     Calculate the start and end index for pagination.
 
@@ -16,40 +15,36 @@ def index_range(page, page_size):
     end_index = start_index + page_size
     return (start_index, end_index)
 
+
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
-     
+
     def dataset(self) -> List[List]:
-
-        """Cached dataset
-        """
-
+        """Cached dataset."""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
-
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ return the appropriate page of the dataset """
+        """Return the appropriate page of the dataset."""
         # check types and values
         assert isinstance(page, int) and page > 0, \
-                "must be a integer greater than 0"
+            "must be a integer greater than 0"
         assert isinstance(page_size, int) and page_size > 0, \
-                "must be a integer greater than 0"
-        
+            "must be a integer greater than 0"
+
         # Get dataset
         dataset = self.dataset()
-        
+
         # Get range indexes
         start, end = index_range(page, page_size)
-        
+
         # Return the slice (empty list if out of range)
         return dataset[start:end]
