bf0fb5e6c735b752779c9058fd1ff077c86a7edc
Ravou <oliviaravou@hotmail.fr>
2025-08-14 13:34:39 +0200

    fix requierement

--- NoSQL/0-list_databases.py
+++ NoSQL/0-list_databases.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-"""
-Script that lists all databases in MongoDB
-"""
+// Script that lists all databases in MongoDB
+
 
 from pymongo import MongoClient
 
 
 def list_databases():
-    # Connect to the MongoDB server (default localhost:27017)
+    // Connect to the MongoDB server (default localhost:27017)
     client = MongoClient()
 
-    # List database names
+    // List database names
     dbs = client.list_database_names()
 
     for db in dbs:
@@ -19,3 +18,4 @@ def list_databases():
 
 if __name__ == "__main__":
     list_databases()
+
