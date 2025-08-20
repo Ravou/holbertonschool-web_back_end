83609dcbd5932d35b36f08f1426044402794dfbc
Ravou <oliviaravou@hotmail.fr>
2025-08-20 15:01:17 +0200

    add task 9

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,8 +1,7 @@
-import getBudgetObject from './7-getBudgetObject.js';
+import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
-
   const fullBudget = {
     ...budget,
     getIncomeInDollars(income) {
@@ -15,4 +14,3 @@ export default function getFullBudgetObject(income, gdp, capita) {
 
   return fullBudget;
 }
-
