2915e4c99043cc7af680d4022244d9a94cb1ab01
Ravou <oliviaravou@hotmail.fr>
2025-08-27 23:23:06 +0200

    add task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,6 +5,13 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
+  ]).then((results) =>
+    results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return { status: 'fulfilled', value: result.value };
+      }
+      return { status: 'rejected', value: result.reason };
+    })
+  );
 }
 
