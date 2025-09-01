d14195b43b12ae63edf3fc1e5a76fcf680c3b7a7
Ravou <oliviaravou@hotmail.fr>
2025-09-01 14:43:43 +0200

    add task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,9 +5,14 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question("Welcome to Holberton School, what is your name? ", function(answer) {
-  console.log(`Your name is: ${answer} `);
-  console.log('This important software is now closing');
-  rl.close()
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.on('line', (input) => {
+  console.log(`Your name is: ${input}`);
+});
+
+rl.on('close', () => {
+   console.log('This important software is now closing');
+   process.exit(0);
 });
 
